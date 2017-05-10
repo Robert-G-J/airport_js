9ea9c681bc67bc9a686d6a469c2209f34b5c1784
Robert Jones <robert.g.jones@gmail.com>
2017-05-10 13:19:24 +0100

    test plane can land at an airport

--- spec/PlaneSpec.js
+++ spec/PlaneSpec.js
@@ -1,10 +1,18 @@
-describe("Plane", function() {
+describe("A plane", function() {
 
   var plane = new Plane();
+  var airport = jasmine.createSpyObj("airport", ["clearForLanding"]);
 
-  describe("#land", function() {
-      it("expects to receive land", function() {
+  describe("can land", function() {
+      it("land with land message", function() {
         expect(plane.land).not.toBeUndefined();
       });
+
+      it("at an airport", function() {
+        plane.land(airport);
+        expect(airport.clearForLanding).toHaveBeenCalledWith(plane);
+      });
+
+
   });
 });
